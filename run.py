c882e6e0545bd7909a6617367e552b8b88c9c796
Joe-Collins-1986 <collins.jw@btinternet.com>
2022-10-12 16:23:38 +0000

    add readme template

--- run.py
+++ run.py
@@ -1,3 +1,7 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+"""
+To use google sheets api will need to install 
+- google-auth
+- gspread
+Instruction video (https://learn.codeinstitute.net/courses/course-v1:CodeInstitute+LS101+2021_T1/courseware/293ee9d8ff3542d3b877137ed81b9a5b/071036790a5642f9a6f004f9888b6a45/?child=first)
+"""
+
