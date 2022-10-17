99aff59a10413666812633b1d4a4d79ee1722608
Joe-Collins-1986 <collins.jw@btinternet.com>
2022-10-17 11:54:43 +0000

    remove testing comments

--- run.py
+++ run.py
@@ -191,75 +191,17 @@ class GameLayout:
                 mines_detected = 0
                 for r in range(max(0, row-1), min(self.board_size, row+2)):
                     for c in range(max(0, col-1), min(self.board_size, col+2)):
-                        #print(r, c)
-
                         if r == row and c == col:
-                            #print("pass")
                             continue
                         elif self.board_layout[r][c] == "*":
                             mines_detected += 1
-                    #print("...")
-                    #print(f"- mines detected: {mines_detected} on this neighboring row")
-                    #print("...")
 
-                #print(f"- TOTAL MINES DETECTED: {mines_detected}")
-                #print("NEXT FIELD")
                 if self.board_layout[row][col] != "*":
                     self.board_layout[row][col] = mines_detected
-            
-            #print("NEXT ROW")
     
         print("\n.............\n") # for testing
         for i in self.board_layout: # for testing
             print(i) # for testing
-
-
-    
-
-
-
-
-
-        """
-        Can't get this to work
-        The above needs to be changed so it is not looping through row and coloumn
-        Below needs to be changed so the max min don;t result in multiple counts.
-        """
-
-        """
-
-        for row in range(self.board_size):
-            for col in range(self.board_size):
-
-                mines_detected = 0
-                if self.board_layout[max(0, row-1)][max(0, col-1)] == "*":
-                        mines_detected +=1
-                if self.board_layout[max(0, row-1)][col] == "*":
-                    mines_detected +=1
-                if self.board_layout[max(0, row-1)][min(self.board_size - 1, col+1)] == "*":
-                    mines_detected +=1
-                if self.board_layout[row][max(0, col-1)] == "*":
-                    mines_detected +=1
-                if self.board_layout[row][min(self.board_size - 1, col+1)] == "*":
-                    mines_detected +=1
-                if self.board_layout[min(self.board_size - 1, row+1)][max(0, col-1)] == "*":
-                    mines_detected +=1
-                if self.board_layout[min(self.board_size - 1, row+1)][col] == "*":
-                    mines_detected +=1
-                if self.board_layout[min(self.board_size - 1, row+1)][min(self.board_size - 1, col+1)] == "*":
-                    mines_detected +=1
-
-                if self.board_layout[row][col] == "*":
-                    continue
-                
-                self.board_layout[row][col] = mines_detected
-            
-
-        print("\n.............\n") # for testing
-        for i in self.board_layout: # for testing
-            print(i) # for testing
-        """
-
 
 
     def selected_space(self):
