83cbe4496120cd6f5ef6bac96e315b2e65ac83e2
Joe-Collins-1986 <collins.jw@btinternet.com>
2022-10-24 11:05:33 +0000

    update html files to present terminal better

--- test.py
+++ test.py
@@ -1,22 +1,3 @@
-import time
-
 print("\U0001F4A5")
 print("\U0001F6A9")
 
-
-start_time = (time.time())
-print(start_time)
-
-time.sleep(63)
-
-stop_time = (time.time())
-print(stop_time)
-
-duration = stop_time - start_time
-print(duration)
-
-hours = int(duration // 60**2)
-mins = int(duration // 60)
-secs = round(duration % 60)
-
-print(f"Hours: {hours} Mins: {mins} Seconds: {secs}")
